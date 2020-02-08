a3a32722dd303cca4f1b377dbd97b9de741c2605
Melusi Khumalo <melusi-k@users.noreply.github.com>
2020-02-08 17:16:56 +0200

    New nav

--- public/src/index.js
+++ public/src/index.js
@@ -1,8 +1,6 @@
 import 'semantic-ui-css/semantic.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 import Firebase, { FirebaseContext } from './components/Firebase';
 require('dotenv').config();
@@ -13,4 +11,3 @@ ReactDOM.render(
   </FirebaseContext.Provider>,
   document.getElementById('root'),
 );
-serviceWorker.register();
