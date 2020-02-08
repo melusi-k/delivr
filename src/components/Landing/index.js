3f67b43050a4b5e862dc7e8bec3738d4491b4e61
Melusi Khumalo <melusi-k@users.noreply.github.com>
2020-02-08 20:45:16 +0200

    Landing page

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -2,34 +2,11 @@ import React from "react";
 import { Icon, Step } from "semantic-ui-react";
 import * as ROUTES from "../../constants/routes";
 import { Link } from "react-router-dom";
-
+import { Flex, Small } from "rebass";
 import "../../App.css";
 
 const App = () => (
   <Step.Group unstackable>
-    <Step active>
-      <Icon name="shipping fast" />
-      <Step.Content>
-        <Step.Title>Shipping</Step.Title>
-        <Step.Description>Describe your items and get a quote</Step.Description>
-      </Step.Content>
-    </Step>
-    <Step active>
-      <Icon name="calendar alternate outline" />
-      <Step.Content>
-        <Step.Title>Confirm</Step.Title>
-        <Step.Description>
-          Confirm details and recieve confirmation
-        </Step.Description>
-      </Step.Content>
-    </Step>
-    <Step active>
-      <Icon name="smile outline" />
-      <Step.Content>
-        <Step.Title>Get Your Goods</Step.Title>
-        <Step.Description>Delivery Delivery Delivery</Step.Description>
-      </Step.Content>
-    </Step>
     <Step active link>
       <Icon name="boxes" />
       <Step.Content>
